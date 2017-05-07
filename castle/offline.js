0ef6bfd63a247de2c2b1dea030d88459d8c798dc
Tomek <tomek@tusonic.pl>
2017-05-07 21:00:42 +0200

    4.0
    CASTLE!

---
+++ castle/offline.js
@@ -0,0 +1,20 @@
+﻿{
+	"version": 1494182725,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"images/tilemap1.png",
+		"images/background1.png",
+		"images/shon-sheet0.png",
+		"images/shon-sheet1.png",
+		"images/shon-sheet2.png",
+		"images/camera-sheet0.png",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
